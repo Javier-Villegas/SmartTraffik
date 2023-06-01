af8854506f2f225e9f6d00ead6de87d6c1bc3bef
Pablo Vera Soto <pabloverasoto99@gmail.com>
2023-06-01 20:37:14 +0200

    Separando escucha de request de escucha de responses

--- maoriot_tracker_2.py
+++ maoriot_tracker_2.py
@@ -40,49 +40,12 @@ def on_discovery(client,userdata,msg):
                                         'rn': 'MAORIOT-AE', 'api':'Dev_tracker','rr':True}}}))
         
 
-# esta función ya no se usa
+
 def on_message_mac(client, userdata, msg):
     print(msg.topic)
     print(msg.payload)
     msg_json = json.loads(msg.payload.decode('utf-8'))
-    mac_address = msg_json['pc']['m2m:cin']['con']
-    node = re.search(r'(?<=Mobius[/])(\w+)(?=[/]new_dev)', msg_json['to']).group(0)
-    print(msg_json)
-    print(node)
-    print(mac_address)
-    #node_address = str(msg.payload)
-    if tracking.get(mac_address):
-        if tracking.get(mac_address) != node:
-            print('Has changed!')
-            rn = tracking.get(mac_address)+'_to_'+node
-            if(rn not in pairs):
-                mqttc.publish('/oneM2M/req/MAORIOT-AE/Mobius2/json',
-                              json.dumps({'to': "Mobius/MAORIOT-AE",'fr': 'MAORIOT-AE','rqi': str(randint(0,10000)),'op':1,'ty':3,
-                                   'pc':{'m2m:cnt':{
-                                        'rn': rn, 'mni':200}}}))
-                time.sleep(1)
-            
-            mqttc.publish('/oneM2M/req/MAORIOT-AE/Mobius2/json',
-                              json.dumps({'to': "Mobius/MAORIOT-AE/"+rn,'fr': 'MAORIOT-AE','rqi': str(randint(0,10000)),'op':1,'ty':4,
-                                   'pc':{'m2m:cin':{
-                                        'con': 1}}}))
-            #mqttc.publish('/oneM2M/req/'+tracking.get(mac_address)+'_to_'+node+'/Mobius2/json', 1)
-    tracking[mac_address] = node
-
-def on_message(client, userdata, msg):
-    print(msg.topic)
-    print(msg.payload)
-    
-    msg_json = json.loads(msg.payload.decode('utf-8'))
-
-    
-
-    if(msg_json['rqi']=="discover_pairs"):
-        for uri in set(msg_json['pc']['m2m:uril']).difference(pairs):
-            if("_to_" in uri):
-                pairs.add(uri)
-
-    elif("m2m:sgn" in msg_json["pc"]):
+    if("m2m:sgn" in msg_json["pc"]): 
         # Publish ack
         mqttc.publish('/oneM2M/resp/Mobius2/MAORIOT-AE/json',
                       json.dumps({'to': 'Mobius', 'fr': 'MAORIOT-AE', 'rqi': msg_json['rqi'], 'rsc': 2000 })) # rsc: 2xxx request received and done, 1xxx request received but in progress
@@ -112,6 +75,20 @@ def on_message(client, userdata, msg):
                 #mqttc.publish('/oneM2M/req/'+tracking.get(mac_address)+'_to_'+node+'/Mobius2/json', 1)
         tracking[mac_address] = node
 
+def on_message(client, userdata, msg):
+    print(msg.topic)
+    print(msg.payload)
+    
+    msg_json = json.loads(msg.payload.decode('utf-8'))
+
+    
+
+    if(msg_json['rqi']=="discover_pairs"):
+        for uri in set(msg_json['pc']['m2m:uril']).difference(pairs):
+            if("_to_" in uri):
+                pairs.add(uri)
+
+    
     
     
 
@@ -127,6 +104,7 @@ mqttc.connect('10.10.10.114')
 mqttc.subscribe('/oneM2M/reg_resp/MAORIOT-AE/+')
 mqttc.subscribe('/oneM2M/resp/MAORIOT-AE/+')
 mqttc.subscribe('/oneM2M/req/+/MAORIOT-AE/+')
+mqttc.message_callback_add('/oneM2M/req/Mobius2/MAORIOT-AE/json',on_message_mac)
 
 
 
