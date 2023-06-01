a785df614527e0d0ae16115885ce2673635f3fed
Javier-Villegas <javiervillegascarrasco@gmail.com>
2023-06-02 00:42:38 +0200

    Discoveries, registration, subscription and interaction placeholder

--- RIC-actuator.py
+++ RIC-actuator.py
@@ -1,40 +1,169 @@
 from paho.mqtt.client import Client
 import json
+import sys
 from time import sleep
 from random import randint
 
-
-def on_discovery(client, userdata, message):
+def on_ric_discovery(client, userdata, message):
+    print('ric discovery')
     print(message.topic)
     print(message.payload)
     msg = json.loads(message.payload)
-    for item in msg['pc']['m2m:uril']:
-        client.subscribe()
-        print(item)
+    if msg.get('pc') and msg['pc'].get('m2m:uril'):
+        if AE_id not in msg['pc']['m2m:uril']:
+            client.publish(f'/oneM2M/reg_req/{AE_id}/Mobius2/json',
+                       json.dumps({'to':csi,
+                                   'fr':AE_id,
+                                   'rqi':AE_id+str(int(randint(0,10000))),
+                                   'op':1,
+                                   'ty':2,
+                                   'pc':{'m2m:ae':{
+                                        'rn':AE_id,
+                                        'api':'RIC-Actuator',
+                                        'rr':True
+                                       }}}))
+        else:
+            print('Reconnecting')
+
+
+
+
+def on_discovery(client, userdata, message):
+    print('on discovery')
+    print(message.topic)
+    print(message.payload)
+    msg = json.loads(message.payload)
+    # Container request
+    if msg.get('rqi') == rqi:
+        print(msg['pc']['m2m:uril'])
+        if msg.get('pc') and msg['pc'].get('m2m:uril'):
+            print(watch_pair)
+            containers = list(filter(lambda x: watch_pair[0] in x or watch_pair[1] in x,
+                                 msg['pc']['m2m:uril']))
+            if len(containers) != 2:
+                print('Tracking path does not exist')
+                exit(1)
+            client.publish(f'/oneM2M/req/{AE_id}/Mobius2/json',
+                       json.dumps({'to':containers[0],
+                                  'fr':AE_id,
+                                  'op':1,
+                                  'ty':2,
+                                  'rqi':f'{AE_id}_{str(randint(0,100000))}',
+                                  'pc':{ 'm2m:sub':{
+                                      'rn': 'sub',
+                                      'nu': [f'mqtt:/{AE_id}'],
+                                      'nct':1,
+                                      'enc':{'net':[3]}
+                                      }
+                                        }}))
+            client.publish(f'/oneM2M/req/{AE_id}/Mobius2/json',
+                       json.dumps({'to':containers[1],
+                                  'fr':AE_id,
+                                  'op':1,
+                                  'ty':2,
+                                  'rqi':f'{AE_id}_{str(randint(0,100000))}',
+                                  'pc':{ 'm2m:sub':{
+                                      'rn': 'sub',
+                                      'nu': [f'mqtt:/{AE_id}'],
+                                      'nct':1,
+                                      'enc':{'net':[3]}
+                                      }
+                                        }}))
+
+
+def on_data_update(client, userdata, message):
+    global last
+    print('New data')
+    print(message.topic)
+    print(message.payload)
+    msg = json.loads(message.payload)
+    client.publish('/oneM2M/resp/Mobius2/MAORIOT-AE/json',
+                      json.dumps({'to':csi,
+                                  'fr':AE_id,
+                                  'rqi': msg['rqi'],
+                                  'rsc': 2000 })) # rsc: 2xxx request received and done, 1xxx request received but in progress
+    idx = msg['pc']['m2m:sgn']['sur'].split('/')[-2]
+    last[idx] = msg['pc']['m2m:sgn']['nev']['rep']['m2m:cin']['con']
+    data[idx].append(last[idx])
+
 
 def on_message(client, userdata, message):
     print(message.topic)
     print(message.payload)
 
 
+
+global last
+# Hardcode for testbed purposes, ideally this would be done with location filters
+watch_pair = [('202481601211147','202481586606346'),
+              ('','')]
+
+watch_pair = watch_pair[int(sys.argv[2])]
+watch_pair = ['_to_'.join(watch_pair), '_to_'.join(watch_pair[::-1])]
+data = {watch_pair[0]:[],watch_pair[1]:[]}
+last = {watch_pair[0]:0,watch_pair[1]:0}
+
+
 server_address = '10.10.10.114'
 csi = 'Mobius'
-AE_id = 'RIC-test'
+AE_id = 'RIC-test'+'_'+sys.argv[1]
 
 
 client = Client()
 client.connect(server_address)
 client.on_message = on_message
-client.subscribe('/oneM2M/resp/+/Mobius2/+')
-client.message_callback_add('/oneM2M/resp/+/Mobius2/+',on_discovery)
+
+client.subscribe(f'/oneM2M/reg_resp/{AE_id}/+')
+client.subscribe(f'/oneM2M/resp/{AE_id}/+')
+client.subscribe(f'/oneM2M/req/+/{AE_id}/+')
+client.message_callback_add(f'/oneM2M/req/Mobius2/{AE_id}/json',on_data_update)
+
+client.message_callback_add(f'/oneM2M/resp/{AE_id}Init/Mobius2/json',on_ric_discovery)
+client.message_callback_add(f'/oneM2M/resp/MAORIOT-AE/Mobius2/json',on_discovery)
+client.subscribe(f'/oneM2M/resp/{AE_id}Init/Mobius2/json')
+client.subscribe(f'/oneM2M/resp/MAORIOT-AE/Mobius2/json')
 
 
-j = json.dumps({'to':csi,'fr':AE_id,'rqi':AE_id,'op':2,'fc':{'fu':1,'ty':2}})
-print(j)
+client.publish(f'/oneM2M/{AE_id}Init/Mobius2/json',
+               json.dumps({'to':csi,
+                           'fr':AE_id,
+                           'rqi':f'{AE_id}_discovery_{str(int(randint(0,100000)))}',
+                           'op':2,
+                           'fc':{'fu':1,'ty':2}}))
+sleep(2)
+rqi = f'{AE_id}_{str(randint(0,10000))}'
 client.publish('/oneM2M/req/MAORIOT-AE/Mobius2/json',
-               json.dumps({'to':'Mobius/MAORIOT-AE','fr':AE_id,'rqi':str(randint(0,10000)),'op':2,'fc':{'fu':1,'ty':3}}))
+               json.dumps({'to':'Mobius/MAORIOT-AE',
+                           'fr':AE_id,
+                           'rqi':rqi,
+                           'op':2,
+                           'fc':{'fu':1,'ty':3}}))
+sleep(2)
 
-sleep(1)
+
+cell_on = False
+client.loop_start()
+while(True):
+    data[watch_pair[0]] = [x for x in data[watch_pair[0]] if x > last[watch_pair[0]]-60000]
+    data[watch_pair[1]] = [x for x in data[watch_pair[1]] if x > last[watch_pair[1]]-60000]
+
+    print(f'{watch_pair[0]}: {len(data[watch_pair[0]])} devices/min')
+    print(f'{watch_pair[1]}: {len(data[watch_pair[1]])} devices/min')
+
+    if True:
+        pass
+    if len(data[watch_pair[0]])+len(data[watch_pair[1]]) > 10 and not cell_on:
+        # Request cell API to turn on
+        pass
+    elif len(data[watch_pair[0]])+len(data[watch_pair[1]]) < 10 and cell_on:
+        # Request cell API to turn off
+        pass
+        
+    
+    last[watch_pair[0]] += 100
+    last[watch_pair[1]] += 100
+    sleep(0.1)
+
 client.loop_forever()
 
 
