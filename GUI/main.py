5e154da6d400b7eb4b4f2297feb228223e5c9623
Carlos Baena <jbghr9k@uma.es>
2023-06-04 14:05:26 +0200

    Implementation of suscription to Mobius

--- GUI/main.py
+++ GUI/main.py
@@ -13,29 +13,37 @@ def on_discovery(client, userdata, msg):
     msg_json = json.loads(msg.payload.decode('utf-8'))
     registered = False
     for uri in set(msg_json['pc']['m2m:uril']).difference(subscriptions):
-        if (uri == "Mobius/GUI-AE"):
-            registered = True
-        else:
-            # mqttc.subscribe(f'/oneM2M/req/{uri.split("/")[1]}/Mobius2/+')
-            # mqttc.message_callback_add(f'/oneM2M/req/{uri.split("/")[1]}/Mobius2/+',on_message_mac)
+        if ("BTNode" in uri):
+            # uri = /Mobius/BTNode_MAC/new_dev
+            aux = uri.split("/")
+            for x in aux:
+                if "BTNode" in x:
+                    nodeName = x
+
             # Create subscription
-            mqttc.publish('/oneM2M/req/MAORIOT-AE/Mobius2/json',
+            mqttc.publish('/oneM2M/req/GUI-test/Mobius2/json',
                           json.dumps(
-                              {'to': uri + "/new_dev", 'fr': 'GUI-AE', 'rqi': str(randint(0, 10000)), 'op': 1,
+                              {'to': uri , 'fr': 'GUI-AE', 'rqi': str(randint(0, 10000)), 'op': 1,
                                'ty': 23,
                                'pc': {'m2m:sub': {
-                                   'rn': 'New_dev_sub', 'nu': ["mqtt:/GUI-AE"], 'nct': 1,
+                                   'rn': 'Discover_device_gui', 'nu': ["mqtt:/GUI-AE"], 'nct': 1,
                                    'enc': {
                                        'net': [3],
                                    }}}}))
             subscriptions.add(uri)
 
-    if (registered == False):
-        mqttc.publish('/oneM2M/reg_req/MAORIOT-AE/Mobius2/json',
-                      json.dumps(
-                          {'to': "Mobius", 'fr': 'MAORIOT-AE', 'rqi': str(randint(0, 10000)), 'op': 1, 'ty': 2,
-                           'pc': {'m2m:ae': {
-                               'rn': 'MAORIOT-AE', 'api': 'GUI_entity', 'rr': True}}}))
+        elif ("MAORIOT-AE" in uri):
+            mqttc.publish('/oneM2M/req/GUI-test/Mobius2/json',
+                          json.dumps(
+                              {'to': uri, 'fr': 'GUI-AE', 'rqi': str(randint(0, 10000)), 'op': 1,
+                               'ty': 23,
+                               'pc': {'m2m:sub': {
+                                   'rn': 'traffic_pattern_gui', 'nu': ["mqtt:/GUI-AE"], 'nct': 1,
+                                   'enc': {
+                                       'net': [3],
+                                   }}}}))
+
+            subscriptions.add(uri)
 
 
 def on_gui_discovery(client, userdata, message):
@@ -67,21 +75,46 @@ def on_message(client, userdata, message):
 
     print(message.topic)
     print(message.payload)
+    msg_json = json.loads(msg.payload.decode('utf-8'))
+
+    if msg_json["pc"].get('m2m:sgn'):
+        # Publish ack
+        mqttc.publish('/oneM2M/resp/Mobius2/GUI-AE/json',
+                      json.dumps({'to': 'Mobius', 'fr': 'GUI-AE', 'rqi': msg_json['rqi'],
+                                  'rsc': 2000}))  # rsc: 2xxx request received and done, 1xxx request received but in progress
+
+
+        # TODO: GET DATA and Plot on GUI
+        # Who trigger the notification
+        node = msg_json['pc']['m2m:sgn']['nev']['rep']['m2m:cin']['cr']
+        if "BTNode" in node:
+            # MAC
+            mac_address = msg_json['pc']['m2m:sgn']['nev']['rep']['m2m:cin']['con']
+            print()
+            pass
+        elif "MAORIOT-AE" in node:
+            subscription_name = msg_json['pc']['m2m:sgn']['sur'] # /Mobius/MAORIOT-AE/<id_path>/<id_subs>
+            # Timestamp
+            timestamp = msg_json['pc']['m2m:sgn']['nev']['rep']['m2m:cin']['con']
+            pass
+
+
+        # node = re.search(r'(?<=Mobius[/])(\w+)(?=[/]new_dev)', msg_json['to']).group(0)
 
     d = getData()  # Get Data from the AEs
 
     # d should be a dict/json with the following parameters: BTNode_1, BTNode_2, RIC_1 and RIC_2
-    if (d != {}):
-        nIVR = n
-        now = datetime.now()
-        d.update({'datetime': now.strftime("%H:%M:%S")})
-
-        datVR = datVR.append(d, ignore_index=True)
-        datVR.reset_index(drop=True, inplace=True)
-        if (len(datVR) == 20):
-            datVR = datVR[1:]
-
-    return nIVR
+    # if (d != {}):
+    #     nIVR = n
+    #     now = datetime.now()
+    #     d.update({'datetime': now.strftime("%H:%M:%S")})
+    #
+    #     datVR = datVR.append(d, ignore_index=True)
+    #     datVR.reset_index(drop=True, inplace=True)
+    #     if (len(datVR) == 20):
+    #         datVR = datVR[1:]
+    #
+    # return nIVR
 
 
 if __name__ == "__main__":
@@ -91,7 +124,7 @@ if __name__ == "__main__":
 
     server_address = '10.10.10.114'
     csi = 'Mobius'
-    AE_id = 'GUI-test' + '_' + sys.argv[1]
+    AE_id = 'GUI-test'
 
     client = Client()
     client.connect(server_address)
@@ -115,7 +148,7 @@ if __name__ == "__main__":
                                'fc': {'fu': 1, 'ty': 2}}))
     rqi = f'{AE_id}_{str(randint(0, 10000))}'
     client.publish(f'/oneM2M/req/{AE_id}/Mobius2/json',
-                   json.dumps({'to': 'Mobius/MAORIOT-AE',
+                   json.dumps({'to': 'Mobius',
                                'fr': AE_id,
                                'rqi': rqi,
                                'op': 2,
